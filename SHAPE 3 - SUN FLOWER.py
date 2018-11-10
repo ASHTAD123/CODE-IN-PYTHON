2b68e2a5dce5a0482a2a8388f8b9e9bab0060056
ASHTAD123 <38254097+ASHTAD123@users.noreply.github.com>
2018-11-10 19:36:17 +0530

    Add files via upload

---
+++ SHAPE 3 - SUN FLOWER.py
@@ -0,0 +1,11 @@
+from turtle import *
+color('orange','yellow')
+pensize(2)
+begin_fill()
+speed(0)
+for  i in range(100):
+    forward(i*5)
+    left(20)
+    left(80)
+end_fill()
+input("prompt: ")
