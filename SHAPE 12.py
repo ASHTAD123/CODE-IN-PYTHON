e1b3f7de3ae6969715bde175070da5e65d455127
ASHTAD123 <38254097+ASHTAD123@users.noreply.github.com>
2018-11-29 19:03:46 +0530

    Add files via upload

---
+++ SHAPE 12.py
@@ -0,0 +1,11 @@
+from turtle import *
+color('RED','YELLOW')
+pensize(2)
+begin_fill()
+speed(0)
+for  i in range(100):
+    forward(i*6)
+    left(55.3)
+    left(80)
+end_fill()
+
---
+++ SHAPE 5 -.py
@@ -0,0 +1,11 @@
+from turtle import *
+color('orange','BLUE')
+pensize(2)
+begin_fill()
+speed(0)
+for  i in range(100):
+    forward(i*6)
+    left(35)
+    left(80)
+end_fill()
+
